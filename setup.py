83e96aa6e3569ac24e711a5b3a23a320904fb6a3
Abdurashidbek Sadullayev <135204006+iosproger@users.noreply.github.com>
2024-12-29 11:17:49 +0000

    update

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # PYTHONPATH=/workspaces/cttest/project python -m uvicorn main:app --host 0.0.0.0 --port 8001
 # from ..schemas.schemas import TaskType
 # PYTHONPATH=/workspaces/cttest/project python -m uvicorn main:app --host 0.0.0.0 --port 8001
+# PYTHONPATH=/workspaces/test/project python -m uvicorn main:app --host 0.0.0.0 --port 8001
 
 # SQLite Viewer
